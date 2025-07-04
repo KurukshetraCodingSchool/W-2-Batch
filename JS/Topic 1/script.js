48770b7e08d2d1d6928e2421ca2c7d6f9b109b2f
kurukshetra <kurukshetracodingschool.com>
2025-07-04 20:36:54 +0530

    JS  is start

---
+++ JS/Topic 1/script.js
@@ -0,0 +1,7 @@
+// var a = 2;
+// var b  =5;
+// // Addition of a two variable
+// var add =  a+b;
+// console.log(add);  
+
+
---
+++ JS/Topic 2/script.js
@@ -0,0 +1,6 @@
+var a = 5;
+let b = 10;
+const c = 12;
+
+var a = 25;
+console.log(a);
